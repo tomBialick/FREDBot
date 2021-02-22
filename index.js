39f25086e5e657c9c9801682df90f0242124f4a2
tom <tb7150@vt.email>
2021-02-21 21:02:51 -0500

    added more get category commands, added param ability

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const TOKEN = authStuff.token
 const FREDTOKEN = authStuff.fredKey
 let config = require('./config.json')
 const fetch = require('node-fetch');
+let prefix = "!"
 
 let fredEndStr = "&api_key="+ FREDTOKEN +"&file_type=json"
 
@@ -14,18 +15,51 @@ bot.on('ready', () => {
   console.info(`Logged in as ${bot.user.tag}!`);
 });
 
-bot.on('message', msg => {
-  if (msg.content === '!help') {
-    msg.channel.send('Here are a list of commands: \n * `!money printer`\n * `!help`\n * `!what`\n * `!get-categories`');
+bot.on('message', message => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+  const args = message.content.slice(prefix.length).trim().split(' ');
+  const command = args.shift().toLowerCase();
+
+  if (command === 'help') {
+    message.channel.send('Here are a list of commands: \n * `!money printer`\n * `!help`\n * `!what`\n * `!get-categories`\n * `!get-subcategories [id]`\n' +
+    +'* `!get-related-categories [id]`\n * `!get-category [id]`');
   }
-  else if (msg.content === '!money printer') {
-    msg.channel.send('BRRRRRRR');
+  else if (command === 'moneyprinter') {
+    message.channel.send('BRRRRRRR');
   }
-  else if (msg.content === '!what') {
-    msg.channel.send('I am a bot that grabs data from the Federal Reserve Economic Data from St Louis!');
+  else if (command === 'what') {
+    message.channel.send('I am a bot that grabs data from the Federal Reserve Economic Data from St Louis!');
   }
-  else if (msg.content === '!get-categories') {
+  else if (command === 'get-categories') {
     fetch( config.fred_url + 'category/children?' + "category_id=0" + fredEndStr, {
+      method: 'GET',
+      headers: {
+          'Content-Type': 'application/json',
+      }
+    }).then(response => response.json()).then((responseJson) => {
+      let outStr = ""
+      for (var i = 0; i < responseJson.categories.length; i++) {
+        outStr += "id: " + responseJson.categories[i].id + "\n" + "name: " + responseJson.categories[i].name + "\n\n"
+      }
+      message.channel.send(outStr);
+    })
+  }
+  else if (command === 'get-subcategories') {
+    fetch( config.fred_url + 'category/children?' + "category_id="+args[0] + fredEndStr, {
+      method: 'GET',
+      headers: {
+          'Content-Type': 'application/json',
+      }
+    }).then(response => response.json()).then((responseJson) => {
+      let outStr = ""
+      for (var i = 0; i < responseJson.categories.length; i++) {
+        outStr += "id: " + responseJson.categories[i].id + "\n" + "name: " + responseJson.categories[i].name + "\n\n"
+      }
+      message.channel.send(outStr);
+    })
+  }
+  else if (command === 'get-related-categories') {
+    fetch( config.fred_url + 'category/related?' + "category_id="+args[0] + fredEndStr, {
       method: 'GET',
       headers: {
           'Content-Type': 'application/json',
@@ -36,7 +70,22 @@ bot.on('message', msg => {
       for (var i = 0; i < responseJson.categories.length; i++) {
         outStr += "id: " + responseJson.categories[i].id + "\n" + "name: " + responseJson.categories[i].name + "\n\n"
       }
-      msg.channel.send(outStr);
+      message.channel.send(outStr);
+    })
+  }
+  else if (command === 'get-category') {
+    fetch( config.fred_url + 'category?' + "category_id="+args[0] + fredEndStr, {
+      method: 'GET',
+      headers: {
+          'Content-Type': 'application/json',
+      }
+    }).then(response => response.json()).then((responseJson) => {
+      console.log(responseJson);
+      let outStr = ""
+      for (var i = 0; i < responseJson.categories.length; i++) {
+        outStr += "id: " + responseJson.categories[i].id + "\n" + "name: " + responseJson.categories[i].name + "\n\n"
+      }
+      message.channel.send(outStr);
     })
   }
 });
