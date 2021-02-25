691967d57b06ae3f3e8dee2a1d25849657635373
tom <tb7150@vt.email>
2021-02-25 15:21:59 -0500

    added get-GNPC-observations command, fixed typo in sample chart, added spaces

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ bot.on('message', message => {
 
   if (command === 'help') {
     message.channel.send('Here are a list of commands: \n * `!money-printer`\n * `!help`\n * `!what`\n * `!get-categories`\n * `!get-subcategories [id]`\n ' +
-    '* `!get-related-categories [id]`\n * `!get-category [id]`');
+    '* `!get-related-categories [id]`\n * `!get-category [id]` \n * `get-GNPC-observations [start YYYY-MM-DD]`');
   }
   else if (command === 'money-printer') {
     message.channel.send('BRRRRRRR');
@@ -47,7 +47,7 @@ bot.on('message', message => {
     })
   }
   else if (command === 'get-subcategories') {
-    fetch( config.fred_url + 'category/children?' + "category_id="+args[0] + fredEndStr, {
+    fetch( config.fred_url + 'category/children?' + "category_id=" + args[0] + fredEndStr, {
       method: 'GET',
       headers: {
           'Content-Type': 'application/json',
@@ -61,7 +61,7 @@ bot.on('message', message => {
     })
   }
   else if (command === 'get-related-categories') {
-    fetch( config.fred_url + 'category/related?' + "category_id="+args[0] + fredEndStr, {
+    fetch( config.fred_url + 'category/related?' + "category_id=" + args[0] + fredEndStr, {
       method: 'GET',
       headers: {
           'Content-Type': 'application/json',
@@ -76,7 +76,7 @@ bot.on('message', message => {
     })
   }
   else if (command === 'get-category') {
-    fetch( config.fred_url + 'category?' + "category_id="+args[0] + fredEndStr, {
+    fetch( config.fred_url + 'category?' + "category_id=" + args[0] + fredEndStr, {
       method: 'GET',
       headers: {
           'Content-Type': 'application/json',
@@ -131,7 +131,7 @@ bot.on('message', message => {
           if (fsWriteError) {
             console.log(fsWriteError);
           } else {
-            message.channel.send("Hey! Here is the chart:", { files: {attachment: './outFolder/chart.jpg',name: 'chart.jpg'}[] })
+            message.channel.send("Hey! Here is the chart:", { files: {attachment: './outFolder/chart.jpg',name: 'chart.jpg'}})
             .then({
               fs.unlink('./outFolder/chart.jpg', (err) => {
                 if (err) throw err;
@@ -144,5 +144,59 @@ bot.on('message', message => {
         console.log(generationError);
       });
     })
+    else if (command === 'get-GNPC-observations') {
+      fetch( config.fred_url + 'series/observations?' + "series_id=GNPCA&observation_end=" + args[0] + fredEndStr, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+      }).then(response => response.json()).then((responseJson) => {
+        // console.log(responseJson);
+        let dataSet = []
+
+        for (var i = 0; i < responseJson.observations.length; i++) {
+          let element = {
+            x: responseJson.observations[i]['date'],
+            value: Number(responseJson.observations[i]['value'])
+          }
+          // dataSet[i].date = responseJson.observations[i]['date']
+          // dataSet[i].value = responseJson.observations[i]['value']
+          dataSet = [...dataSet, element]
+        }
+
+        // Create instance of JSDOM.
+        var jsdom = new JSDOM('<body><div id="container"></div></body>', {runScripts: 'dangerously'});
+        // Get window
+        var window = jsdom.window;
+        // require anychart and anychart export modules
+        var anychart = require('anychart')(window);
+        var anychartExport = require('anychart-nodejs')(anychart);
+
+        // create and a chart to the jsdom window.
+        // chart creating should be called only right after anychart-nodejs module requiring
+        var chart = anychart.line(dataSet);
+        chart.bounds(0, 0, 1000, 1000);
+        chart.container('container');
+        chart.draw();
+
+        // generate JPG image and save it to a file
+        anychartExport.exportTo(chart, 'jpg').then(function(image) {
+          fs.writeFile('./outFolder/chart.jpg', image, function(fsWriteError) {
+            if (fsWriteError) {
+              console.log(fsWriteError);
+            } else {
+              message.channel.send("Hey! Here is the chart:", { files: {attachment: './outFolder/chart.jpg',name: 'chart.jpg'}})
+              .then({
+                fs.unlink('./outFolder/chart.jpg', (err) => {
+                  if (err) throw err;
+                });
+              })
+              .catch(console.error);
+            }
+          });
+        }, function(generationError) {
+          console.log(generationError);
+        });
+      })
   }
 });
